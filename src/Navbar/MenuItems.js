ccc526b967d49259e5870ef8f29a9a1f043b03f5
Antynor <grontman.anita@gmail.com>
2021-04-23 21:12:45 +0200

    Force merge!

---
+++ src/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+const MenuItems = [
+    {
+        title:'Home',
+        url: '# ',
+        cName:'nav-links'
+    },
+    {
+        title:'Example',
+        url: '# ',
+        cName:'nav-links'
+    },
+    {
+        title:'Gallery',
+        url: '# ',
+        cName:'nav-links'
+    },
+    {
+        title:'Contact',
+        url: '# ',
+        cName:'nav-links'
+    }
+]
+export default MenuItems
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React,{Component} from 'react';
+//import { Button } from './Button';
+import MenuItems from './MenuItems';
+import './Navbar.css'
+class Navbar extends Component {
+    state ={
+         cliked: false
+        }
+        handleClick = () =>{
+            this.setState({ cliked: !this.state.cliked})
+        }
+    render() {
+      
+  
+      return (
+        <header>
+          <nav className="navBar">
+          <h1 className="navbarLogo">Flowers <i className="fab fa-pagelines"></i>   </h1>
+          <div className="icon" onClick={this.handleClick}>
+              <i className={this.state.cliked ? 'fas fa-times' : 'fas fa-bars'}> </i>
+          </div>
+              <ul className={this.state.cliked ? 'nav-menu active' : 'nav-menu'}>
+                 
+                  {MenuItems.map((item, index) =>{
+                      return(
+                  
+                <li key={index}>
+                    <a className={item.cName} href={item.url}>
+                    {item.title}
+                    </a>
+                </li>)
+    })}
+              </ul>
+            </nav>  
+            
+        </header>
+      );
+    }
+  }
+  
+  export default Navbar;
